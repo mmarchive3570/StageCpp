a0c15489f9c6c7e4dc0dffff6d4408eaee5eafbd
MARCHIVE <mael.marchive@gmail.com>
2025-09-04 10:48:52 +0200

    ajout du main stageDescription.cpp

---
+++ stageDescription.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.14.36414.22 d17.14
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "satgeDescription", "satgeDescription.vcxproj", "{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}.Debug|x64.ActiveCfg = Debug|x64
+		{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}.Debug|x64.Build.0 = Debug|x64
+		{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}.Debug|x86.ActiveCfg = Debug|Win32
+		{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}.Debug|x86.Build.0 = Debug|Win32
+		{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}.Release|x64.ActiveCfg = Release|x64
+		{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}.Release|x64.Build.0 = Release|x64
+		{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}.Release|x86.ActiveCfg = Release|Win32
+		{063D64E8-A4AA-47D9-A9F7-2A2D98A694A2}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {5B0FE153-66C0-4FBC-ABB8-0D8A6C257AB2}
+	EndGlobalSection
+EndGlobal
